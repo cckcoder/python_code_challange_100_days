54a37ff43040eeedf8733b9b24aed1c8e7863dcb
codewiz <c.chaiwat.ch@gmail.com>
2021-06-15 07:48:35 +0700

    add: day 1 practice.

---
+++ day_1/day_1.py
@@ -0,0 +1,29 @@
+#E x.1
+print('Hello')
+
+# Ex.2
+print('Hello world')
+
+#Ex.3
+print('hello_python')
+
+#Ex.4
+print("Merry X'Mas")
+
+#Ex.5
+print('I want to ask you "Why don\'t you drive to work?"')
+
+#Ex.6
+print('\"I don\'t have a car\"')
+
+#Ex.7
+print('You got a new job!? That\'s so exciting')
+
+#Ex.8
+print('สวัสดีวันจันทร์')
+
+#Ex.9
+print('ความแตกต่างระหว่างคนเก่งกับคนไม่เก่ง คือ \"การใช้เวลาว่าง\nให้เป็นประโยชน์')
+
+#Ex.10
+# print("/\/\/\")
