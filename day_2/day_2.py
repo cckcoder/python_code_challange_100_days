6d3573db75b35b8e957f1ef432b6a9f05cbf668c
codewiz <c.chaiwat.ch@gmail.com>
2021-06-16 07:47:58 +0700

    update: day 2

---
+++ day_2/day_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""
+    %d %f %s
+"""
+print('%d' %3) # Print Integer
+
+print('%f' %1) # Print Float
+
+print('%.2f' %3) # Print Float but config decimal
+
+print('%s' %99) # General what your input it show same.
+
+print('%s' %7.99)
+
+print('%d %.3f %s' %(2, 5, 3.21))
+
+print('I Love you', 3000)
+
+print('5 + 7 =', 5 + 7)
+
---
+++ day_2/exercise_d_2.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+print("/\\/\\/\\")
+
+# Ex11
+print("a\nan\nant")
+
+
+# Ex12
+print("a\nan\nant")
+
+# Ex12
+print("""
+*    +    *
++    *    +
+*    +    *
+""")
+
+
+# Ex12
+print("""
+*    +    *
++    *    +
+*    +    *
+""")
+
+# Ex13
+print("""
+Just because something
+thinks differently from you,
+dose that mean it's not thinking?
+""")
+
+
+# Ex14
+print("""
+\\      /
+    *
+/      \\
+""")
+
+# Ex12
+print("""
+*    +    *
++    *    +
+*    +    *
+""")
+
